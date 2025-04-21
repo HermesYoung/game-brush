8607111e1fedecfa60b68313a1e54bf266e5e076
HermesYoung <tails777tw@gmail.com>
2025-04-21 23:37:58 +0800

    add preset of an ellipse object

---
+++ src/entities/shapes/EllipseGameObject.ts
@@ -0,0 +1,29 @@
+﻿import {PhysicsBasedObject} from "../PhysicsBasedObject";
+
+export abstract class EllipseGameObject extends PhysicsBasedObject{
+
+    radiusX: number;
+    radiusY: number;
+    rotation: number;
+    protected constructor(position: {x: number, y: number}, radiusX: number, radiusY: number, rotation: number) {
+        super(position);
+        this.radiusX = radiusX;
+        this.radiusY = radiusY;
+        this.rotation = rotation;
+    }
+
+    update(deltaTime: number): void {
+        throw new Error("Method not implemented.");
+    }
+    draw(ctx: CanvasRenderingContext2D): void {
+        ctx.save();
+        ctx.translate(this.position.x, this.position.y);
+        ctx.rotate(this.rotation);
+        ctx.beginPath();
+        ctx.ellipse(0, 0, this.radiusX, this.radiusY, 0, 0, Math.PI * 2);
+        ctx.strokeStyle = "red";
+        ctx.stroke();
+        ctx.restore();
+    }
+
+}
